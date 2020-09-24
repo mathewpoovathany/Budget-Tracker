f2f1efbabaf8255174f541e316642594b309485e
Mathew Poovathany <poovathanymathew@gmail.com>
2020-09-24 16:20:26 -0400

    modified thedb connections to flecilble to heroku

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://nromano1-user:Sunset!88@ds263590.mlab.com:63590/heroku_3zvsv4kd", 
-  { 
-    useMongoClient: true, 
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
+ {  useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+ });
 
 // routes
 app.use(require("./routes/api.js"));
